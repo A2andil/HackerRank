dea4ff8525b8a12b105e77116dc60a21f81b7e8f
A2andil <A77mad.22andil@gmail.com>
2020-09-26 02:14:02 +0200

    write a function

---
+++ Python/write-a-function.py
@@ -0,0 +1,13 @@
+#IN THE NAME OF ALLAH
+
+def is_leap(year):
+    leap = False
+    if year % 4 == 0 and (year % 400 == 0 or year % 100 != 0):
+        leap = True
+    else:
+        leap = False
+    
+    return leap
+
+year = int(raw_input())
+print is_leap(year)
