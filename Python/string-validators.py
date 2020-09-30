91e7c580a0086506ea4adaf7c9e54f219031dc61
A2andil <A77mad.22andil@gmail.com>
2020-09-30 12:20:48 +0200

    String Validators

---
+++ Python/string-validators.py
@@ -0,0 +1,9 @@
+#In The Name of Allah
+
+if __name__ == '__main__':
+    str = input()
+    print(any(c.isalnum()  for c in str))
+    print(any(c.isalpha() for c in str))
+    print(any(c.isdigit() for c in str))
+    print(any(c.islower() for c in str))
+    print(any(c.isupper() for c in str))
